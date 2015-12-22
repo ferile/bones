fbb72e12ae1437f8ca9331997575a31f1afc4539
Andy Fairlie <afairlie@gmail.com>
2015-12-22 13:35:21 +0200

    Updating IE stylesheet reference

--- library/bones.php
+++ library/bones.php
@@ -131,13 +131,13 @@ function bones_scripts_and_styles() {
 		wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/css/style.css', array(), '', 'all' );
 
 		// ie-only style sheet
-		wp_register_style( 'bones-ie-only', get_stylesheet_directory_uri() . '/library/css/ie.css', array(), '' );
+		wp_register_style( 'bones-ie-only', get_stylesheet_directory_uri() . '/library/css/style-legacy.css', array(), '' );
 
 	// comment reply script for threaded comments
 	if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
 		  wp_enqueue_script( 'comment-reply' );
 	}
-
+ 
 		//adding scripts file in the footer
 		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/scripts/script.min.js', array( 'jquery' ), '', true );
 
