978d06c154cdd1b6c32bcff1570df1a0a73c8e1a
Andy Fairlie <afairlie@gmail.com>
2015-12-22 08:44:30 +0200

    Updated FE framework Removed Bones SCSS files Updated columns Added Editor styles and login styles

--- includes/_page-start.php
+++ includes/_page-start.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
  
 <?php if ( is_active_sidebar( 'sidebar1' ) ) : ?> 
-<div class="column-wrapper">
-	<main id="site-content" class="column column-66 clearfix" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
+<div class="column-wrapper medium-column-wrapper"> 
+	<main id="site-content" class="column column-70 / medium-column-60 / clearfix" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+ 
 <?php else : ?>
 <main id="site-content" class="clearfix" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-<?php endif; ?>
+<?php endif; ?> 
--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
-	<div id="sidebar" class="sidebar column column-34 clearfix" role="complementary">
+	<div id="sidebar" class="sidebar / column column-30 / medium-column-40 / clearfix" role="complementary">
 		<h2 class="visually-hidden">Sidebar</h2>
 		<?php dynamic_sidebar( 'sidebar1' ); ?>
 	</div>
